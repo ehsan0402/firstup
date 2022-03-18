24b2c09258845f1e6e6d0a8ce3347e8f6d4c8910
EhSaN <ehsan@EhSaNEs-MacBook-Air.local>
2022-03-18 20:38:34 +0330

    redirect

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,9 +1,14 @@
+import { Button } from "antd";
 import StyleW from "../../components/layouts/defaultLayout/wrapper/style";
 
 export function NotFound(){
+    function Home (){
+        <Redirect to="/" />
+    }
     return(
         <StyleW>
         <p>404</p>
+        <p onClick={Home}>Home</p>
         </StyleW>
     )
 }
